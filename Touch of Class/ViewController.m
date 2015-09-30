749e35a45f4ebf3f18ac772f569947f95bf2fd96
Jay Versluis <jay@versluis.com>
2015-09-29 20:54:26 -0400

    implemented touches began

--- Touch of Class/ViewController.m
+++ Touch of Class/ViewController.m
@@ -10,6 +10,10 @@
 
 @interface ViewController ()
 
+@property (strong, nonatomic) IBOutlet UILabel *positionLabel;
+@property (strong, nonatomic) IBOutlet UILabel *forceLabel;
+
+
 @end
 
 @implementation ViewController
@@ -22,6 +26,32 @@
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)trackTouch:(UITouch *)touch {
+    
+    // grab the current position
+    CGPoint position = [touch locationInView:self.view];
+    NSString *xPosition = [NSString stringWithFormat:@"%f", position.x];
+    NSString *yPosition = [NSString stringWithFormat:@"%f", position.y];
+    
+    // update the labels
+    NSString *positionText = [NSString stringWithFormat:@"X = %@ / Y = %@", xPosition, yPosition];
+    self.positionLabel.text = positionText;
+}
+
+#pragma mark - Responders
+
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+    
+    // is it a touch?
+    if (event.type == UIEventTypeTouches) {
+        
+        // for each touch update the label
+        for (UITouch *currentTouch in touches) {
+            [self trackTouch:currentTouch];
+        }
+    }
 }
 
 @end
