45a02a5533fa2b45467ecc0bb417d96dca2be80f
Jay Versluis <jay@versluis.com>
2015-09-29 20:27:33 -0400

    Initial Commit

---
+++ Touch of Class/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Touch of Class
+//
+//  Created by Jay Versluis on 29/09/2015.
+//  Copyright © 2015 Pinkstone Pictures LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Touch of ClassTests/Touch_of_ClassTests.m
@@ -0,0 +1,39 @@
+//
+//  Touch_of_ClassTests.m
+//  Touch of ClassTests
+//
+//  Created by Jay Versluis on 29/09/2015.
+//  Copyright © 2015 Pinkstone Pictures LLC. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Touch_of_ClassTests : XCTestCase
+
+@end
+
+@implementation Touch_of_ClassTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
